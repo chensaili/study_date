4092faea0a1c4160e7e1008d27ecf38b75057019
chensaili <chensaili.dxy.cn>
2020-02-09 09:16:03 +0800

    利用minus()方法计算一年前的日期，利用plus()方法计算一年后的日期

---
+++ src/Sample8_LocalDate.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Sample8_LocalDate {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+
+        LocalDate previousYear = today.minus(1, ChronoUnit.YEARS);
+        System.out.println("一年前的日期 : " + previousYear);
+
+        LocalDate nextYear = today.plus(1, ChronoUnit.YEARS);
+        System.out.println("一年后的日期:"+nextYear);
+    }
+}
